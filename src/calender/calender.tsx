c137ad0524ac93b30843c561a342baa41a0a23aa
Riya Patel <riyapatel@Riyas-MacBook-Air.local>
2020-12-15 11:39:57 +0530

    added arrow and scroll effect in calender

--- src/calender/calender.tsx
+++ src/calender/calender.tsx
@@ -2,15 +2,17 @@ import React, { useEffect } from "react";
 import "./calender.css";
 import moment from "moment";
 import renderCalender from "./renderCalender";
+import arrowImage from "../assets/images/arrow.png";
 
 function Calender() {
   const weeksHeader = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const [calender, setCalender] = React.useState([]);
   const [todaysDate, setTodaysDate] = React.useState(moment());
+  const [renderCount, setRenderCount] = React.useState(0);
 
   useEffect(() => {
     setCalender(renderCalender(todaysDate));
-  }, [todaysDate]);
+  }, [todaysDate, renderCount]);
 
   function isToday(day) {
     return todaysDate.isSame(day, "day");
@@ -32,56 +34,71 @@ function Calender() {
     return todaysDate.clone().add(1, "month");
   }
 
+  function throttleScrol(date) {
+    let isScrolling;
+    window.clearTimeout(isScrolling);
+    // Set a timeout to run after scrolling ends
+    isScrolling = setTimeout(function () {
+      // Run the callback
+      setTodaysDate(date);
+      setRenderCount(renderCount + 1);
+    }, 650);
+  }
+
   return (
     <div className="container calenderContainer">
-      <div className="card">
-        <div className="py-4 d-flex align-items-center card-header">
-          <div className="px-4">
-            <p
-              className="mb-0"
-              style={{ cursor: "pointer" }}
-              onClick={() => setTodaysDate(prevMonth())}
-            >
-              Prev
-            </p>
-          </div>
-          <div className="flex-grow-1 text-center px-4">
-            {currentMonth()} - {currentYear()}
-          </div>
-          <div className="px-4">
-            <p
-              className="mb-0"
-              style={{ cursor: "pointer" }}
-              onClick={() => setTodaysDate(nextMonth())}
-            >
-              Next
-            </p>
-          </div>
+      <div className="d-flex align-items-center card-header header-height">
+        <div className="flex-grow-1 px-4">
+          {currentMonth()} - {currentYear()}
         </div>
-        <div className="card-body">
-          <div className="calender py-4">
-            {weeksHeader.map((header) => (
-              <p className="mb-0 text-center" key={header}>
-                {header}
-              </p>
+        <div
+          className="ml-auto arrowButtonWrapper d-flex align-items-center justify-content-center"
+          onClick={() => setTodaysDate(prevMonth())}
+        >
+          <img src={arrowImage} alt="arrow" className="previousArrow" />
+        </div>
+        <button className="btn" onClick={() => setTodaysDate(moment())}>
+          Today
+        </button>
+        <div
+          className="arrowButtonWrapper d-flex align-items-center justify-content-center"
+          onClick={() => setTodaysDate(nextMonth())}
+        >
+          <img src={arrowImage} alt="arrow" className="nextArrow" />
+        </div>
+      </div>
+      <div className="calender py-4">
+        {weeksHeader.map((header) => (
+          <p className="mb-0 text-center" key={header}>
+            {header}
+          </p>
+        ))}
+      </div>
+      <div
+        className="body-height"
+        onWheel={(event: any) => {
+          if (event.nativeEvent.wheelDelta > 0) {
+            throttleScrol(prevMonth());
+          } else {
+            throttleScrol(nextMonth());
+          }
+        }}
+      >
+        {calender.map((week, index: number) => (
+          <div key={index} className="calender">
+            {week.map((day, index: number) => (
+              <div
+                key={index}
+                className="cell d-flex justify-content-end"
+                onClick={() => setTodaysDate(day)}
+              >
+                <p className={`mb-0 ${isToday(day) ? "todaysDate" : "date"}`}>
+                  {day.format("D").toString()}
+                </p>
+              </div>
             ))}
           </div>
-          {calender.map((week, index: number) => (
-            <div key={index} className="calender">
-              {week.map((day, index: number) => (
-                <div
-                  key={index}
-                  className="cell d-flex justify-content-end"
-                  onClick={() => setTodaysDate(day)}
-                >
-                  <p className={`mb-0 ${isToday(day) ? "todaysDate" : "date"}`}>
-                    {day.format("D").toString()}
-                  </p>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+        ))}
       </div>
     </div>
   );

e02e6a1a8be2dac3ff8877758f199c2f6c55c6c2
Shrey Patel <shreynpatel23@gmail.com>
2020-12-16 17:57:20 +0530

    added api for the posts (#2)
    Co-authored-by: Riya Patel <riyapatel@Riyas-MacBook-Air.local>

---
+++ src/api/requestObject.ts
@@ -0,0 +1,47 @@
+export const obj = {
+    "RequestObjects": [
+        {
+            "Post": {
+                "OperationType": "Read",
+                "Privacy": {
+                    "SearchValues": [
+                        "Public"
+                    ],
+                    "Return": true
+                },
+                "UserId": {
+                    "SearchValues": [
+                        "assign"
+                    ],
+                    "Return": false
+                },
+                "id": {
+                    "Return": true
+                },
+                "IsCalendarEntry": {
+                    "SearchValues": [
+                        true
+                    ],
+                    "Return": true
+                },
+                "Images": {
+                    "Return": true
+                },
+                "Text": {
+                    "Return": true
+                },
+                "Rating": {
+                    "Return": true
+                },
+                "TypeOfDay": {
+                    "Return": true
+                },
+                "CalendarDateTime": {
+                    "Return": true,
+                    "Sort": "Descending" 
+                },
+                "ContinuationToken": null
+            }
+        }
+    ]
+}
--- src/calender/calender.tsx
+++ src/calender/calender.tsx
@@ -3,12 +3,65 @@ import "./calender.css";
 import moment from "moment";
 import renderCalender from "./renderCalender";
 import arrowImage from "../assets/images/arrow.png";
+import getAllInvites from "../api/getAllInvites";
 
 function Calender() {
   const weeksHeader = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const [calender, setCalender] = React.useState([]);
   const [todaysDate, setTodaysDate] = React.useState(moment());
   const [renderCount, setRenderCount] = React.useState(0);
+  const [posts, setPosts] = React.useState([
+    {
+      date: "",
+      images: [],
+      rating: 0,
+      text: "",
+      typeOfDay: [],
+    },
+  ]);
+
+  useEffect(() => {
+    async function getAllPosts() {
+      try {
+        await getAllInvites(40)
+          .then((res: any) => {
+            res.data.ResponseObjects.map((data) => {
+              const { Posts } = data;
+              const arr = [];
+              Posts.map((post) => {
+                const {
+                  CalendarDateTime,
+                  Images,
+                  Rating,
+                  Text,
+                  TypeOfDay,
+                } = post;
+                arr.push({
+                  date: moment(CalendarDateTime).format("L"),
+                  images: Images,
+                  rating: Rating,
+                  text: Text,
+                  typeOfDay: TypeOfDay,
+                });
+                return arr;
+              });
+              const removeDuplicateDates = arr.filter(
+                (value, index, array) =>
+                  array.findIndex((arr) => arr.date === value.date) === index
+              );
+              return setPosts(removeDuplicateDates);
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getAllPosts();
+  }, []);
 
   useEffect(() => {
     setCalender(renderCalender(todaysDate));
@@ -42,7 +95,28 @@ function Calender() {
       // Run the callback
       setTodaysDate(date);
       setRenderCount(renderCount + 1);
-    }, 650);
+    }, 800);
+  }
+
+  function getLegendStyle(legend) {
+    if (legend === "Protein Treatment") return "rgb(221,235,241)";
+    if (legend === "Hair Color") return "rgb(244,223,235)";
+    if (legend === "Hair Cut") return "rgb(244,223,235)";
+    if (legend === "Deep Conditioning") return "rgb(221,237,234)";
+    if (legend === "Clarifying") return "rgb(251,228,228)";
+  }
+
+  function getLegendInitials(legend) {
+    if (legend === "Protein Treatment") return "Pr";
+    if (legend === "Hair Color") return "HC";
+    if (legend === "Hair Cut") return "Cu";
+    if (legend === "Deep Conditioning") return "DC";
+    if (legend === "Clarifying") return "C";
+  }
+
+  function viewPost(event) {
+    event.stopPropagation();
+    console.log("please share the view of this post");
   }
 
   return (
@@ -57,7 +131,10 @@ function Calender() {
         >
           <img src={arrowImage} alt="arrow" className="previousArrow" />
         </div>
-        <button className="btn" onClick={() => setTodaysDate(moment())}>
+        <button
+          className="mx-2 btn btn-light"
+          onClick={() => setTodaysDate(moment())}
+        >
           Today
         </button>
         <div
@@ -89,12 +166,46 @@ function Calender() {
             {week.map((day, index: number) => (
               <div
                 key={index}
-                className="cell d-flex justify-content-end"
+                className="cell"
                 onClick={() => setTodaysDate(day)}
               >
                 <p className={`mb-0 ${isToday(day) ? "todaysDate" : "date"}`}>
                   {day.format("D").toString()}
                 </p>
+                {posts?.map((post, i: number) => {
+                  if (post.date.includes(day.format("L"))) {
+                    return (
+                      <div key={i} onClick={(e) => viewPost(e)}>
+                        <div className="py-2 d-flex">
+                          {post.typeOfDay.map((legend, index: number) => {
+                            return (
+                              <div
+                                key={index}
+                                className="legend mr-1 d-flex align-items-center justify-content-center"
+                                style={{ background: getLegendStyle(legend) }}
+                              >
+                                <p className="mb-0 legendText">
+                                  {getLegendInitials(legend)}
+                                </p>
+                              </div>
+                            );
+                          })}
+                        </div>
+                        <div
+                          style={{ width: "100%", height: "83px" }}
+                          className="d-flex align-items-center justify-content-center"
+                        >
+                          <img
+                            src={post.images[0].ImageUrl}
+                            alt="thumbnail_image"
+                            style={{ height: "100%" }}
+                          />
+                        </div>
+                      </div>
+                    );
+                  }
+                  return null;
+                })}
               </div>
             ))}
           </div>

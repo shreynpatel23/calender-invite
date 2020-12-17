2a0e317d465b5d5ab4f749fbdab611007cf0f3f3
Shrey Patel <shreynpatel23@gmail.com>
2020-12-17 16:40:22 +0530

    added tile navigation and smooth scrolling (#3)
    Co-authored-by: Riya Patel <riyapatel@Riyas-MacBook-Air.local>

--- src/calender/calender.tsx
+++ src/calender/calender.tsx
@@ -4,12 +4,15 @@ import moment from "moment";
 import renderCalender from "./renderCalender";
 import arrowImage from "../assets/images/arrow.png";
 import getAllInvites from "../api/getAllInvites";
+import Tile from "./tile/tile";
 
 function Calender() {
   const weeksHeader = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const [calender, setCalender] = React.useState([]);
   const [todaysDate, setTodaysDate] = React.useState(moment());
   const [renderCount, setRenderCount] = React.useState(0);
+  const [toggleTileView, setToggleTileView] = React.useState(false);
+  const [selectedPost, setSelectedPost] = React.useState({});
   const [posts, setPosts] = React.useState([
     {
       date: "",
@@ -37,7 +40,7 @@ function Calender() {
                   TypeOfDay,
                 } = post;
                 arr.push({
-                  date: moment(CalendarDateTime).format("L"),
+                  date: moment(CalendarDateTime).format("Do MMM YYYY"),
                   images: Images,
                   rating: Rating,
                   text: Text,
@@ -114,103 +117,114 @@ function Calender() {
     if (legend === "Clarifying") return "C";
   }
 
-  function viewPost(event) {
-    event.stopPropagation();
-    console.log("please share the view of this post");
-  }
-
   return (
-    <div className="container calenderContainer">
-      <div className="d-flex align-items-center card-header header-height">
-        <div className="flex-grow-1 px-4">
-          {currentMonth()} - {currentYear()}
-        </div>
-        <div
-          className="ml-auto arrowButtonWrapper d-flex align-items-center justify-content-center"
-          onClick={() => setTodaysDate(prevMonth())}
-        >
-          <img src={arrowImage} alt="arrow" className="previousArrow" />
-        </div>
-        <button
-          className="mx-2 btn btn-light"
-          onClick={() => setTodaysDate(moment())}
-        >
-          Today
-        </button>
-        <div
-          className="arrowButtonWrapper d-flex align-items-center justify-content-center"
-          onClick={() => setTodaysDate(nextMonth())}
-        >
-          <img src={arrowImage} alt="arrow" className="nextArrow" />
-        </div>
-      </div>
-      <div className="calender py-4">
-        {weeksHeader.map((header) => (
-          <p className="mb-0 text-center" key={header}>
-            {header}
-          </p>
-        ))}
-      </div>
-      <div
-        className="body-height"
-        onWheel={(event: any) => {
-          if (event.nativeEvent.wheelDelta > 0) {
-            throttleScrol(prevMonth());
-          } else {
-            throttleScrol(nextMonth());
-          }
-        }}
-      >
-        {calender.map((week, index: number) => (
-          <div key={index} className="calender">
-            {week.map((day, index: number) => (
-              <div
-                key={index}
-                className="cell"
-                onClick={() => setTodaysDate(day)}
-              >
-                <p className={`mb-0 ${isToday(day) ? "todaysDate" : "date"}`}>
-                  {day.format("D").toString()}
-                </p>
-                {posts?.map((post, i: number) => {
-                  if (post.date.includes(day.format("L"))) {
-                    return (
-                      <div key={i} onClick={(e) => viewPost(e)}>
-                        <div className="py-2 d-flex">
-                          {post.typeOfDay.map((legend, index: number) => {
-                            return (
-                              <div
-                                key={index}
-                                className="legend mr-1 d-flex align-items-center justify-content-center"
-                                style={{ background: getLegendStyle(legend) }}
-                              >
-                                <p className="mb-0 legendText">
-                                  {getLegendInitials(legend)}
-                                </p>
-                              </div>
-                            );
-                          })}
-                        </div>
-                        <div
-                          style={{ width: "100%", height: "83px" }}
-                          className="d-flex align-items-center justify-content-center"
-                        >
-                          <img
-                            src={post.images[0].ImageUrl}
-                            alt="thumbnail_image"
-                            style={{ height: "100%" }}
-                          />
-                        </div>
-                      </div>
-                    );
-                  }
-                  return null;
-                })}
-              </div>
-            ))}
+    <div>
+      <div className="container calenderContainer">
+        <div className="d-flex align-items-center card-header header-height">
+          <div className="flex-grow-1 px-4">
+            {currentMonth()} - {currentYear()}
           </div>
-        ))}
+          <div
+            className="ml-auto arrowButtonWrapper d-flex align-items-center justify-content-center"
+            onClick={() => setTodaysDate(prevMonth())}
+          >
+            <img src={arrowImage} alt="arrow" className="previousArrow" />
+          </div>
+          <button
+            className="mx-2 btn btn-light"
+            onClick={() => setTodaysDate(moment())}
+          >
+            Today
+          </button>
+          <div
+            className="arrowButtonWrapper d-flex align-items-center justify-content-center"
+            onClick={() => setTodaysDate(nextMonth())}
+          >
+            <img src={arrowImage} alt="arrow" className="nextArrow" />
+          </div>
+        </div>
+        <div className="calender py-4">
+          {weeksHeader.map((header) => (
+            <p className="mb-0 text-center" key={header}>
+              {header}
+            </p>
+          ))}
+        </div>
+        <div
+          className="body-height"
+          onWheel={(event: any) => {
+            if (event.nativeEvent.wheelDelta > 0) {
+              throttleScrol(prevMonth());
+            } else {
+              throttleScrol(nextMonth());
+            }
+          }}
+        >
+          {calender.map((week, index: number) => (
+            <div key={index} className="calender">
+              {week.map((day, index: number) => (
+                <div
+                  key={index}
+                  className="cell"
+                  onClick={() => setTodaysDate(day)}
+                >
+                  <p className={`mb-0 ${isToday(day) ? "todaysDate" : "date"}`}>
+                    {day.format("D").toString()}
+                  </p>
+                  {posts?.map((post, i: number) => {
+                    if (post.date.includes(day.format("Do MMM YYYY"))) {
+                      return (
+                        <div
+                          key={i}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            setSelectedPost(post);
+                            setToggleTileView(true);
+                          }}
+                        >
+                          <div className="py-2 d-flex">
+                            {post.typeOfDay.map((legend, index: number) => {
+                              return (
+                                <div
+                                  key={index}
+                                  className="legend mr-1 d-flex align-items-center justify-content-center"
+                                  style={{ background: getLegendStyle(legend) }}
+                                >
+                                  <p className="mb-0 legendText">
+                                    {getLegendInitials(legend)}
+                                  </p>
+                                </div>
+                              );
+                            })}
+                          </div>
+                          <div
+                            style={{ width: "100%", height: "83px" }}
+                            className="d-flex align-items-center justify-content-center"
+                          >
+                            <img
+                              src={post.images[0].ImageUrl}
+                              alt="thumbnail_image"
+                              style={{ height: "100%" }}
+                            />
+                          </div>
+                        </div>
+                      );
+                    }
+                    return null;
+                  })}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
+      {toggleTileView ? (
+        <Tile
+          posts={posts}
+          click={() => setToggleTileView(false)}
+          selectedPost={selectedPost}
+        />
+      ) : null}
     </div>
   );
 }
